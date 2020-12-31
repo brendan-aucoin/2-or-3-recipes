d018f0f1d50dbe8b08138674b1c1c9fa99e456c1
Brendan <brendan.aucoin@ryerson.ca>
2020-12-31 15:41:51 -0500

    my first commit

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+  state ={
+
+  }
+
+  render(){
+    // we will return the menu bar, and the routes to all our other pages, using react router, and the Routes
+    return (
+      <div className = "App">
+    
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/my-app/src/components/AddRecipe.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class AddRecipe extends Component{
+    state = {
+
+    }
+
+    render(){
+        // contains all the components of the add recipe page
+        return (
+            <div className = "Add-Recipe">
+
+            </div>
+        );
+    }
+}
+
+export default AddRecipe;
