df07d6341f877f807a17d335e59cb867a6f47ff6
Brendan <brendan.aucoin@ryerson.ca>
2021-01-17 00:39:16 -0500

    test

---
+++ controllers/apiController.js
@@ -0,0 +1,68 @@
+const RecipeModel = require('../schemas/Recipe');
+const upload = require('../image-uploading/storage.js');
+
+module.exports = function(app,urlencodedParser){
+
+
+    // app.get('/*',(req,res)=>{
+    //     res.sendFile(path.join(__dirname,'build','index.html'));
+    // })
+    // the function where you add a new recipe to the database
+    // you need the upload.single as the middleware
+    app.post("/upload-recipe",urlencodedParser,upload.single('recipeImage'),(req,res)=>{
+        // take the requests body and convert it from a string to a js object to send to the database
+        const recipeContent = JSON.parse(req.body.content);
+
+        // you dont wanna include certain state properties in the data base 
+        const deleteProperties = JSON.parse(req.body.deleteProperties);
+        deleteProperties.forEach(property=>delete recipeContent[property]);
+
+        // changing the prep,cook time,servings,and calories to numbers
+        const convertToNumberProperties = JSON.parse(req.body.convertToNumberProperties);
+        convertToNumberProperties.forEach(property=>convertStringToNumber(recipeContent,property))
+
+        
+        // if the user didnt include a file then just input null for the database image path
+        let picture = req.file ? req.file.path : null;
+        // on the database it turned \\(correct) to \(incorrect) so to fix you replace \\ with / ie: uploads\\dog.jpg => uploads/dog.jpg
+        if(picture){
+            picture = picture.replace("\\",'/');
+        }
+
+        // create a new object with the content from the requests body, the picture path, and the date
+        const savedRecipe = {
+            ...recipeContent,
+            picture:picture,
+            date:new Date()
+        }
+
+        console.log(savedRecipe);
+        
+        // save the new recipe you made to the database
+        let newRecipe = new RecipeModel(savedRecipe);
+        newRecipe.save();
+    });
+
+
+    app.get('/get-all-recipes',(req,res)=>{
+        RecipeModel.find({},(err,data)=>{
+            if(err){throw err}
+            // console.log(data);
+            res.json(data);
+        })
+    });
+
+    app.get('/get-recipe/:recipeID',(req,res)=>{
+        RecipeModel.findById((req.params.recipeID)).then(doc=>{
+            res.json({recipe:doc})
+        }).catch(err=>{
+            console.log(err);
+        })
+    });
+}
+
+
+const convertStringToNumber = (object,property)=>{
+    const newNumber = Number(object[property]);
+    object[property] = (newNumber && newNumber>0) ? newNumber :null;
+}
---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const apiController = require('./controllers/apiController');
+const connectMongoose = require('./database/connection');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// const whitelist = ['http://localhost:3000','http://localhost:5000','https://shrouded-journey-38552.herokuapp.com']
+// const corsOptions = {
+//     origin:function(origin,callback){
+//         console.log("** Origin of reques " + origin)
+//         if(whitelist.indexOf(origin) !== -1 || !origin){
+//             console.log("Origin acceptable")
+//             callback(null,true);
+//         }
+//         else{
+//             console.log("Origin rejected")
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
+
+// app.use(cors(corsOptions))
+app.use(bodyParser.json());
+app.use('/uploads',express.static('uploads'));
+
+
+//mongoose
+connectMongoose();
+
+const urlencodedParser = bodyParser.urlencoded({extended:false})
+
+if(process.env.NODE_ENV === 'production'){
+    //serve any static files
+    app.use(express.static(path.join(__dirname,'my-app/build')));
+    //handle react routing and returns al react apps
+    app.get('*',function(req,res){
+        res.sendFile(path.join(__dirname,'../frontend/my-app/build','index.html'))
+    });
+}
+apiController(app,urlencodedParser);
+console.log(`Listening to PORT: ${PORT}`);
+
+
+app.listen(PORT);
+
+
